55af473320ea9f3b495adf747a0c8b297e343e3c
Marko Rintamäki <marko.rintamaki@gmail.com>
2017-09-13 10:25:18 +0300

    mysql installation

--- installation/amazon/configAmazon.js
+++ installation/amazon/configAmazon.js
@@ -5,7 +5,7 @@ global.config = {
 		hostname: 'localhost',
 		user: 'openeditplay',
 		database: 'openeditplay',
-		password: fs.readFileSync('../.pw', 'utf8'),
+		password: fs.readFileSync('../.pw', 'utf8').split('\n')[0],
 		port: 3306
 	}
 };
