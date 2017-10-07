70b623e7f83da5dc18dfb45cad7f952ea511a9f9
Marko Rintamäki <marko.rintamaki@gmail.com>
2017-10-07 15:11:39 +0300

    front page css fixes

--- public/js/gameElement.js
+++ public/js/gameElement.js
@@ -1,28 +1,31 @@
 const sizeExplanation = 'Game size in data units. Every object in scene, type, component, level and property is a data unit.';
 
 class GameList {
-    constructor() {
+	constructor() {
 		let el = redom.el;
 
-		this.el = el('table.gameList',
-			el('thead.gameListHeader',
-				el('th.gameListHeaderName', 'Name'),
-				el('th.gameListHeaderPlay'),
-				el('th.gameListHeaderSize', 'Size', { title: sizeExplanation }),
-				el('th.gameListHeaderLevels', 'Levels'),
-				el('th.gameListHeaderCreated', 'Created'),
-				el('th.gameListHeaderModified', 'Modified')
-			),
-			this.list = redom.list('tbody.gameListContent', Game)
+		this.el = el('div.gameListContainer',
+			el('table.gameList',
+				el('thead.gameListHeader',
+					el('th.gameListHeaderName', 'Name'),
+					el('th.gameListHeaderPlay'),
+					el('th.gameListHeaderSize', 'Size', {title: sizeExplanation}),
+					el('th.gameListHeaderLevels', 'Levels'),
+					el('th.gameListHeaderCreated', 'Created'),
+					el('th.gameListHeaderModified', 'Modified')
+				),
+				this.list = redom.list('tbody.gameListContent', Game)
+			)
 		);
-    }
-    update(gameListData) {
+	}
+
+	update(gameListData) {
 		this.list.update(gameListData);
-    }
+	}
 }
 
 class Game {
-    constructor() {
+	constructor() {
 		let el = redom.el;
 
 		this.el = el('tr.gameElement',
@@ -33,25 +36,26 @@ class Game {
 				}),
 				this.editButton = el('a.linkButton.editButton', 'Edit')
 			),
-			this.size = el('td.gameElementSize', { title: sizeExplanation }),
+			this.size = el('td.gameElementSize', {title: sizeExplanation}),
 			this.levels = el('td.gameElementLevels'),
 			this.created = el('td.gameElementCreated'),
 			this.modified = el('td.gameElementModified')
 		);
-    }
-    update(gameData) {
+	}
+
+	update(gameData) {
 		this.name.textContent = gameData.name;
 		this.size.textContent = gameData.serializableCount;
 		this.levels.textContent = gameData.levelCount;
-		
+
 		this.created.textContent = dateToAgoFormat(gameData.createdAt);
 		this.created.setAttribute('title', new Date(gameData.createdAt).toLocaleString());
 
 		this.modified.textContent = dateToAgoFormat(gameData.updatedAt);
 		this.modified.setAttribute('title', new Date(gameData.updatedAt).toLocaleString());
-		
+
 		this.playButton.setAttribute('href', '/play/?gameId=' + gameData.id);
 		this.editButton.setAttribute('href', '/edit/?gameId=' + gameData.id);
-    }
+	}
 }
 
