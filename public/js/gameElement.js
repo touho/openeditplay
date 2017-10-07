4c1a935aa22729e59e8780fb1c7d39a206c5a06b
Marko Rintamäki <marko.rintamaki@gmail.com>
2017-10-07 14:24:58 +0300

    super mobile friendly front page game scrolling

--- public/js/gameElement.js
+++ public/js/gameElement.js
@@ -1,3 +1,5 @@
+const sizeExplanation = 'Game size in data units. Every object in scene, type, component, level and property is a data unit.';
+
 class GameList {
     constructor() {
 		let el = redom.el;
@@ -5,10 +7,11 @@ class GameList {
 		this.el = el('table.gameList',
 			el('thead.gameListHeader',
 				el('th.gameListHeaderName', 'Name'),
-				el('th.gameListHeaderSize', 'Size'),
+				el('th.gameListHeaderPlay'),
+				el('th.gameListHeaderSize', 'Size', { title: sizeExplanation }),
+				el('th.gameListHeaderLevels', 'Levels'),
 				el('th.gameListHeaderCreated', 'Created'),
-				el('th.gameListHeaderModified', 'Modified'),
-				el('th.gameListHeaderPlay')
+				el('th.gameListHeaderModified', 'Modified')
 			),
 			this.list = redom.list('tbody.gameListContent', Game)
 		);
@@ -24,20 +27,22 @@ class Game {
 
 		this.el = el('tr.gameElement',
 			this.name = el('td.gameElementName'),
-			this.size = el('td.gameElementSize'),
-			this.created = el('td.gameElementCreated'),
-			this.modified = el('td.gameElementModified'),
 			this.play = el('td.gameElementPlay',
 				this.playButton = el('a.linkButton.playButton', 'Play', {
 					onclick: standaloneMobileLinkClickEventSupport
 				}),
 				this.editButton = el('a.linkButton.editButton', 'Edit')
-			)
+			),
+			this.size = el('td.gameElementSize', { title: sizeExplanation }),
+			this.levels = el('td.gameElementLevels'),
+			this.created = el('td.gameElementCreated'),
+			this.modified = el('td.gameElementModified')
 		);
     }
     update(gameData) {
 		this.name.textContent = gameData.name;
 		this.size.textContent = gameData.serializableCount;
+		this.levels.textContent = gameData.levelCount;
 		
 		this.created.textContent = dateToAgoFormat(gameData.createdAt);
 		this.created.setAttribute('title', new Date(gameData.createdAt).toLocaleString());
